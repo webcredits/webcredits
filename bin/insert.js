8d4efe5552a1d1afa9b8dc2f1c08004240588888
Melvin Carvalho <melvincarvalho@gmail.com>
2016-01-22 20:23:10 +0100

    commander for insert

--- bin/insert.js
+++ bin/insert.js
@@ -14,48 +14,54 @@ function bin(argv) {
 
   var credit = {};
 
-  credit["@type"]                                = 'https://w3id.org/cc#Credit';
-  credit["https://w3id.org/cc#source"]           = argv[2];
-  credit["https://w3id.org/cc#amount"]           = argv[3];
-  credit["https://w3id.org/cc#currency"]         = argv[4];
-  credit["https://w3id.org/cc#destination"]      = argv[5];
-  credit["http://purl.org/dc/terms/description"] = argv[6];
-  credit["https://w3id.org/cc#timestamp"]        = argv[7];
-
-
-  // clean and validate
-  if (!credit["https://w3id.org/cc#source"]) {
-    console.error('source is required');
-    process.exit(-1);
-  }
-
-  if (!credit["https://w3id.org/cc#amount"] || isNaN(credit["https://w3id.org/cc#amount"]) ) {
-    console.error('amount is required and must be a number');
-    process.exit(-1);
-  }
-
-  if (!credit["https://w3id.org/cc#currency"]) {
-    credit["https://w3id.org/cc#currency"] = 'https://w3id.org/cc#bit';
-  }
-
-  if (!credit["https://w3id.org/cc#destination"]) {
-    console.error('destination is required');
-    process.exit(-1);
-  }
-
   program
+  .arguments('<source> <amount> <currency> <destination> [description] [timestamp]')
   .option('-d, --database <database>', 'Database')
   .option('-w, --wallet <wallet>', 'Wallet')
+  .action(function(source, amount, currency, destination, description, timestamp, options){
+    credit["@type"]                                = 'https://w3id.org/cc#Credit';
+    credit["https://w3id.org/cc#source"]           = source;
+    credit["https://w3id.org/cc#amount"]           = amount;
+    credit["https://w3id.org/cc#currency"]         = currency;
+    credit["https://w3id.org/cc#destination"]      = destination;
+    credit["http://purl.org/dc/terms/description"] = description;
+    credit["https://w3id.org/cc#timestamp"]        = timestamp;
+
+
+    // clean and validate
+    if (!credit["https://w3id.org/cc#source"]) {
+      console.error('source is required');
+      process.exit(-1);
+    }
+
+    if (!credit["https://w3id.org/cc#amount"] || isNaN(credit["https://w3id.org/cc#amount"]) ) {
+      console.error('amount is required and must be a number');
+      process.exit(-1);
+    }
+
+    if (!credit["https://w3id.org/cc#currency"]) {
+      credit["https://w3id.org/cc#currency"] = 'https://w3id.org/cc#bit';
+    }
+
+    if (!credit["https://w3id.org/cc#destination"]) {
+      console.error('destination is required');
+      process.exit(-1);
+    }
+
+
+    var defaultDatabase = 'webcredits';
+    var defaultWallet   = 'https://localhost/wallet/test#this';
+
+    config.database = program.database || config.database || defaultDatabase;
+    config.wallet   = program.wallet   || config.wallet   || defaultWallet;
+
+    var sequelize = webcredits.setupDB(config);
+    webcredits.insert(credit, sequelize, config);
+
+  })
   .parse(argv);
 
-  var defaultDatabase = 'webcredits';
-  var defaultWallet   = 'https://localhost/wallet/test#this';
 
-  config.database = program.database || config.database || defaultDatabase;
-  config.wallet   = program.wallet   || config.wallet   || defaultWallet;
-
-  var sequelize = webcredits.setupDB(config);
-  webcredits.insert(credit, sequelize, config);
 
 }
 
