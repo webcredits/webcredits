c6b5fc2c5c4869fb0fa25287226abb8521e6776f
Melvin Carvalho <melvincarvalho@gmail.com>
2016-09-13 01:45:47 +0200

    Add deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+npm --no-git-tag-version version patch
+
 git add -u
 git commit -m "$1"
 git push origin master
+
+git checkout gh-pages
+git merge master
+git push origin gh-pages
+
+git checkout master
+
+npm publish
+
